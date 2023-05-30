1df1f5802f35a10e52e82195eb8b62bac7669d06
MiruClay <1140106918@qq.com>
2023-05-30 18:50:53 +0800

    Add files via upload

--- nonebot_plugin_RandomTouhouPictures/__init__.py
+++ nonebot_plugin_RandomTouhouPictures/__init__.py
@@ -1,1 +1,17 @@
+from nonebot.adapters.onebot.v11 import Bot, Message
+from nonebot import on_keyword
+from nonebot.typing import T_State
+from nonebot.adapters import Bot,Event
+import requests
 
+pic=on_keyword({'看东方图'})
+@pic.handle()
+async def _(bot:Bot,event:Event,state:T_State):
+    msg = await suijitu()
+    await pic.send(Message(msg))
+
+async def suijitu():
+    url='https://img.paulzzh.com/touhou/random'
+    pic = requests.get(url)
+    pic_ti = f"[CQ:image,file={pic.url}]"
+    return pic_ti
